a80f7230d60766b60beeff8d327cb01ec0c7db91
lizuoqun <2645929971@qq.com>
2022-02-10 21:54:38 +0800

    主从复制与读写分离、公共表

---
+++ src/main/java/com/lzq/shardingjdbc/entity/Common.java
@@ -0,0 +1,14 @@
+package com.lzq.shardingjdbc.entity;
+
+import lombok.Data;
+
+/**
+ * @author lizuoqun
+ * @version 1.0
+ * @date 2022-02-10 21:32
+ */
+@Data
+public class Common {
+    private Long commonId;
+    private String remark;
+}
